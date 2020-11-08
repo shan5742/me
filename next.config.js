d90a1aa5c7cab64e047c3a358e403412b08fd48a
shan5742 <asamshan456@gmail.com>
2020-11-08 19:08:47 +0000

    setting up build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withImages = require("next-images");
 module.exports = withImages({
+  target: "serverless",
   webpack: function (config) {
     config.module.rules.push({
       test: /\.md$/,
