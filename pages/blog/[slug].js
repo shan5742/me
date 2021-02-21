d7cd9b688a6c9f71fecd50caeb40470ed7ecbd9c
asam1 <asam@libraryofthings.co.uk>
2021-02-21 22:47:32 +0000

    adding scroll to top button

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -7,22 +7,34 @@ function PostTemplate({ content, data }) {
   // This holds the data between `---` from the .md file
   const frontmatter = data;
 
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+
   return (
-    <div className="markdown max-w-3xl mt-12 px-8 flex flex-col mx-auto">
-      <h1>{frontmatter.title}</h1>
-      <ReactMarkdown source={content} />
-      <div className="flex">
-        {frontmatter.tags.map((t, i) => (
-          <p key={i} className="bg-secondary text-primary px-4 py-2 m-2">
-            {t}
-          </p>
-        ))}
+    <div className="relative">
+      <div className="markdown max-w-3xl mt-12 px-8 flex flex-col mx-auto">
+        <h1>{frontmatter.title}</h1>
+        <ReactMarkdown source={content} />
+        <div className="flex">
+          {frontmatter.tags.map((t, i) => (
+            <p key={i} className="bg-secondary text-primary px-4 py-2 m-2">
+              {t}
+            </p>
+          ))}
+        </div>
+        <Link href="/blog">
+          <a className="text-accent text-md font-bold hover:underline">
+            back to blog
+          </a>
+        </Link>
       </div>
-      <Link href="/blog">
-        <a className="text-accent text-md font-bold hover:underline">
-          back to blog
-        </a>
-      </Link>
+      <div
+        className="sticky bottom-0 right-0 pr-8 pb-8 text-right text-accent cursor-pointer"
+        onClick={scrollToTop}
+      >
+        scroll to top
+      </div>
     </div>
   );
 }
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -2,6 +2,7 @@ import fs from "fs";
 import matter from "gray-matter";
 import moment from "moment";
 import Link from "next/link";
+import { FaArrowUp } from "react-icons/fa";
 
 export default function Blog({ posts }) {
   const sortedBlogPosts = posts.sort((a, b) => {
@@ -11,52 +12,65 @@ export default function Blog({ posts }) {
       ? 1
       : 0;
   });
+
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
   return (
-    <div className="max-w-3xl px-8 mt-12 mx-auto">
-      <h1 className="text-accent text-3xl md:text4xl lg:text-5xl xl:text-5xl font-bold my-4">
-        Blog Posts
-      </h1>
-      <Link href="/">
-        <a className="text-accent text-md font-bold hover:underline">
-          back to home page
-        </a>
-      </Link>
-      {sortedBlogPosts.map(
-        ({ frontmatter: { title, description, date, tags }, slug }) => (
-          <article
-            className="bg-secondary py-12 px-8 rounded-md my-8"
-            key={title}
-          >
-            <header>
-              <h3 className="mb-2">
-                <Link href={"/blog/[slug]"} as={`/blog/${slug}`}>
-                  <a className="text-3xl font-semibold text-accent no-underline">
-                    {title}
-                  </a>
-                </Link>
-              </h3>
-              <span className="mb-8 text-sm text-secondary">
-                {moment(date).format("LL")}
-              </span>
-            </header>
-            <section>
-              <p className="my-8 text-primary">{description}</p>
-            </section>
-            <div className="flex flex-wrap">
-              {tags.map((t, i) => (
-                <p key={i} className="bg-primary text-accent px-4 py-2 m-2">
-                  #{t}
-                </p>
-              ))}
-            </div>
-          </article>
-        )
-      )}
-      <Link href="/">
-        <a className="text-accent text-md font-bold hover:underline">
-          back to home page
-        </a>
-      </Link>
+    <div className="relative">
+      <div className="max-w-3xl px-8 mt-12 mx-auto">
+        <h1 className="text-accent text-3xl md:text4xl lg:text-5xl xl:text-5xl font-bold my-4">
+          Blog Posts
+        </h1>
+        <Link href="/">
+          <a className="text-accent text-md font-bold hover:underline">
+            back to home page
+          </a>
+        </Link>
+        {sortedBlogPosts.map(
+          ({ frontmatter: { title, description, date, tags }, slug }) => (
+            <article
+              className="bg-secondary py-12 px-8 rounded-md my-8"
+              key={title}
+            >
+              <header>
+                <h3 className="mb-2">
+                  <Link href={"/blog/[slug]"} as={`/blog/${slug}`}>
+                    <a className="text-3xl font-semibold text-accent no-underline">
+                      {title}
+                    </a>
+                  </Link>
+                </h3>
+                <span className="mb-8 text-sm text-secondary">
+                  {moment(date).format("LL")}
+                </span>
+              </header>
+              <section>
+                <p className="my-8 text-primary">{description}</p>
+              </section>
+              <div className="flex flex-wrap">
+                {tags.map((t, i) => (
+                  <p key={i} className="bg-primary text-accent px-4 py-2 m-2">
+                    #{t}
+                  </p>
+                ))}
+              </div>
+            </article>
+          )
+        )}
+
+        <Link href="/">
+          <a className="text-accent text-md font-bold hover:underline">
+            back to home page
+          </a>
+        </Link>
+      </div>
+      <div
+        className="sticky bottom-0 right-0 pr-8 pb-8 text-right text-accent cursor-pointer"
+        onClick={scrollToTop}
+      >
+        scroll to top
+      </div>
     </div>
   );
 }
